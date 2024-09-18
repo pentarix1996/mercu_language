fc14137a0ea34821f24db117554cf3df7da1e8fc
Antonio.Carvajal <antonio.carva.96@gmail.com>
2024-09-18 15:25:24 +0200

    ADD Tokens file

---
+++ tokens.py
@@ -0,0 +1,46 @@
+EOF = 'EOF' # End of file
+
+# Types
+NUMBER = 'NUMBER'
+BOOLEAN = "BOOLEAN"
+STRING = 'STRING'
+
+# Special Values
+TRUE = 'TRUE'
+FALSE = 'FALSE'
+
+# Unitary Operators
+IDENTIFIER = 'IDENTIFIER' # Var and functions
+ASSIGN = 'ASSIGN' # =
+
+# Binary Operators
+MUL = 'MUL'
+DIV = 'DIV'
+
+# Unitary/Bynary Operators
+PLUS = 'PLUS'
+MINUS = 'MINUS'
+
+# Syntax
+LPAREN = 'LPAREN' # (
+RPAREN = 'RPAREN' # )
+COMMA = 'COMMA'
+LBRACE = 'LBRACE' # {
+RBRACE = 'RBRACE' # }
+COLON = 'COLON' # :
+LBRACKET = 'LBRACKET' # [
+RBRACKET = 'RBRACKET' # ]
+IF = 'IF'
+ELIF = 'ELIF'
+ELSE = 'ELSE'
+
+# Logic Operators
+AND = 'AND'
+OR = 'OR'
+NOT = 'NOT'
+EQUALS = 'EQUALS' # ==
+NOT_EQUALS = 'NOT_EQUALS' # !=
+LESS_THAN = 'LESS_THAN' # <
+GREATER_THAN = 'GREATER_THAN' # >
+LESS_EQUAL = 'LESS_EQUAL' # <=
+GREATER_EQUAL = 'GREATER_EQUAL' # >=
